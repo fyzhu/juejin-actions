2ef53fa5b2273a8274cba5e3166cf6079b6beb48
fyzhu <i@zhuyunfeng.com>
2024-08-14 11:55:02 +0800

    add cookie

--- test/env.js
+++ test/env.js
@@ -1,7 +1,7 @@
 // 此文件存放所有环境变量
 
 // 掘金cookie
-export const JUEJIN_COOKIE = '';
+export const JUEJIN_COOKIE = 'sid_guard=fdea64a16a818f8c91e5e4a4d0aec970%7C1692980942%7C31536000%7CSat%2C+24-Aug-2024+16%3A29%3A02+GMT; uid_tt=9f955e6bdfcb5e592a58b283b2d87086; uid_tt_ss=9f955e6bdfcb5e592a58b283b2d87086; sid_tt=fdea64a16a818f8c91e5e4a4d0aec970; sessionid=fdea64a16a818f8c91e5e4a4d0aec970; sessionid_ss=fdea64a16a818f8c91e5e4a4d0aec970; sid_ucp_v1=1.0.0-KDM0ODljYmRkY2NmYWUwZTc2NmZiNzBkMDdmNTQyYTc4N2Q2ZTBjNWUKFwieouG__fWBAxDOraOnBhiwFDgCQO8HGgJsZiIgZmRlYTY0YTE2YTgxOGY4YzkxZTVlNGE0ZDBhZWM5NzA; ssid_ucp_v1=1.0.0-KDM0ODljYmRkY2NmYWUwZTc2NmZiNzBkMDdmNTQyYTc4N2Q2ZTBjNWUKFwieouG__fWBAxDOraOnBhiwFDgCQO8HGgJsZiIgZmRlYTY0YTE2YTgxOGY4YzkxZTVlNGE0ZDBhZWM5NzA; store-region=cn-bj; store-region-src=uid; _tea_utm_cache_2608=undefined; __tea_cookie_tokens_2608=%257B%2522user_unique_id%2522%253A%25227244182731365762617%2522%252C%2522web_id%2522%253A%25227244182731365762617%2522%252C%2522timestamp%2522%253A1693640597217%257D; _tea_utm_cache_576092=undefined; csrf_session_id=3beacdaebdc418ab2e113ecf4699577e';
 
 /*************** 邮箱服务使用 ***************/
 // 发送方邮箱
