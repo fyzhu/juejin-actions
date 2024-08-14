88b5a8b40a637d590ec98347c906260771e2ad2a
fyzhu <i@zhuyunfeng.com>
2024-08-14 11:54:08 +0800

    add user-agent

--- src/config/index.js
+++ src/config/index.js
@@ -4,6 +4,7 @@ export const headers = {
     "accept": "*/*",
     "accept-language": "zh-CN,zh;q=0.9",
     "content-type": "application/json",
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36 Edg/127.0.0.0',
     "sec-ch-ua": "\" Not;A Brand\";v=\"99\", \"Google Chrome\";v=\"97\", \"Chromium\";v=\"97\"",
     "sec-ch-ua-mobile": "?0",
     "sec-ch-ua-platform": "\"Windows\"",
