e8d15467084df3b21f2b2add3086baa7a5cc6c5a
fyzhu <i@zhuyunfeng.com>
2024-08-14 11:48:20 +0800

    add empty object

--- src/services/index.js
+++ src/services/index.js
@@ -30,14 +30,14 @@ export const getStatus = async function(){
  */
 export const checkIn = async function(){
     return await request({
-        url: `${prefix}check_in`,
+        url: `${prefix}check_in?aid=2608&uuid=7244182731365762617&spider=0&msToken=ZfB76UE8OVvtR5pVFjq-HKfGn87zO3mG1ae75JXahP9eP3j3Z91UseGxC6EA6quL1JgXkU3srTOVODDodxgXw3x5bFHQDKFZUKI95QV7m5R0gQBgdxBTGyxahPuexiw%3D&a_bogus=df4dhO2OMsm1wXVfOwDz9y8Ex3u0YW4bgZENCs8KDzLS`,
         method: 'post',
         data: {
-            aid:2608,
-            uuid:7244182731365762617,
-            spider:0,
-            msToken:'ZfB76UE8OVvtR5pVFjq-HKfGn87zO3mG1ae75JXahP9eP3j3Z91UseGxC6EA6quL1JgXkU3srTOVODDodxgXw3x5bFHQDKFZUKI95QV7m5R0gQBgdxBTGyxahPuexiw%3D'
-            ,a_bogus:'df4dhO2OMsm1wXVfOwDz9y8Ex3u0YW4bgZENCs8KDzLS'
+            // aid:2608,
+            // uuid:7244182731365762617,
+            // spider:0,
+            // msToken:'ZfB76UE8OVvtR5pVFjq-HKfGn87zO3mG1ae75JXahP9eP3j3Z91UseGxC6EA6quL1JgXkU3srTOVODDodxgXw3x5bFHQDKFZUKI95QV7m5R0gQBgdxBTGyxahPuexiw%3D'
+            // ,a_bogus:'df4dhO2OMsm1wXVfOwDz9y8Ex3u0YW4bgZENCs8KDzLS'
         }
     });
 }
@@ -71,7 +71,8 @@ export const getCurPoint = async function(){
  */
 export const freeDraw = async function(){
     return await request({
-        url: `${prefix}lottery/draw`,
-        method: 'post'
+        url: `${prefix}lottery/draw?aid=2608&uuid=7244182731365762617&spider=0&msToken=gcX9HGQmxv6x6PTquHaMrTkaBIyM4rAHc9k1vyq7p2_2XHlrGvD_hEY_LJQzopb7NLXy3-1hWOKYlHOGuL1ObFxTVjbDxp2kl3qnhRSy0V6TMpxCuzE7oMAAfgUUlLs%3D&a_bogus=E6UmvOZVMsm1MhVfRXDz9emExIg0YW59gZENEhiyJzLK`,
+        method: 'post', 
+        data: {}
     });
 }
